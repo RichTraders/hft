40a975d9fabe60aa8072e7f84e8e22064d7a7233
JB <withbini2057@naver.com>
2025-08-25 19:52:00 +0900

    TRAD-193 Fix logger test

--- test/logger_tests.cpp
+++ test/logger_tests.cpp
@@ -10,10 +10,10 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
  */
 
-#include "logger.h"
+#include <cerrno>
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include <cerrno>
+#include "logger.h"
 
 using namespace common;
 
@@ -62,6 +62,7 @@ TEST(LoggerTest, ConsoleLogTest) {
 }
 
 TEST(LoggerTest, FileLogTest) {
+  //init
   std::string cur_dir_path = getCurrentWorkingDirectory();
   std::string remove_dir = cur_dir_path + "/file_log_test.txt";
 
@@ -89,7 +90,7 @@ TEST(LoggerTest, FileLogTest) {
 
   EXPECT_TRUE(ifs.is_open());
 
-  for (uint i =0;i < line_list.size();i++) {
+  for (uint i = 0; i < line_list.size(); i++) {
     std::string line;
     if (std::getline(ifs, line)) {
       EXPECT_TRUE(line.find(line_list[i]) != std::string::npos);
@@ -98,9 +99,11 @@ TEST(LoggerTest, FileLogTest) {
 }
 
 TEST(LoggerTest, FileLogLotateTest) {
-  std::string cur_dir_path = getCurrentWorkingDirectory();
-  std::string remove_dir1 = cur_dir_path + "/file_log_lotate_test_final.txt";
-  std::string remove_dir2 = cur_dir_path + "/file_log_lotate_test_final_1.txt";
+  const std::string cur_dir_path = getCurrentWorkingDirectory();
+  const std::string file_path = "file_log_rotate_test_final.txt";
+  const std::string file_path2 = "file_log_rotate_test_final_1.txt";
+  const std::string remove_dir1 = cur_dir_path + file_path;
+  const std::string remove_dir2 = cur_dir_path + file_path2;
 
   std::ifstream remove_file(remove_dir1);
 
@@ -113,16 +116,14 @@ TEST(LoggerTest, FileLogLotateTest) {
     std::remove(remove_dir2.c_str());
 
   std::vector<std::string> line_list;
-  line_list.push_back("FileLogTest lotate Test");
-  line_list.push_back("Application lotate shutting down333");
-  const std::string file_path = "file_log_lotate_test_final.txt";
-  const std::string file_path2 = "file_log_lotate_test_final_1.txt";
+  line_list.push_back("FileLogTest rotate Test");
+  line_list.push_back("Application rotate shutting down333");
 
   Logger lg;
 
   lg.setLevel(LogLevel::kDebug);
   lg.clearSink();
-  lg.addSink(std::make_unique<FileSink>("file_log_lotate_test_final", 32));
+  lg.addSink(std::make_unique<FileSink>("file_log_rotate_test_final", 32));
 
   lg.debug(line_list[0].c_str());
   lg.debug(line_list[1].c_str());
@@ -142,13 +143,25 @@ TEST(LoggerTest, FileLogLotateTest) {
   }
 
   {
-    std::ifstream ifs(file_path2);
+    std::ifstream ifs(file_path);
 
     EXPECT_TRUE(ifs.is_open());
 
     std::string line;
     if (std::getline(ifs, line)) {
       int ssss = line.find(line_list[1]) != std::string::npos;
+      EXPECT_TRUE(ssss);
+    }
+    ifs.close();
+  }
+  {
+    std::ifstream ifs(file_path2);
+
+    EXPECT_TRUE(ifs.is_open());
+
+    std::string line;
+    if (std::getline(ifs, line)) {
+      int ssss = line.find(line_list[0]) != std::string::npos;
       EXPECT_TRUE(ssss);
     }
     ifs.close();
@@ -162,7 +175,7 @@ TEST(LoggerTest, FileAndConsoleLogTest) {
   std::ifstream remove_file(remove_dir);
 
   if (remove_file.is_open())
-      std::remove(remove_dir.c_str());
+    std::remove(remove_dir.c_str());
 
   Logger lg;
   lg.setLevel(LogLevel::kDebug);
@@ -183,14 +196,11 @@ TEST(LoggerTest, FileAndConsoleLogTest) {
   }
   sleep(3);
 
-  // 3) std::cout 버퍼 복원
   std::cout.rdbuf(old_cout_buf);
 
-  // 4) 캡처된 문자열을 istringstream 에 담아 std::cin 으로 연결
-    std::istringstream iss(oss.str());
+  std::istringstream iss(oss.str());
   auto* old_cin_buf = std::cin.rdbuf(iss.rdbuf());
 
-  // 5) 이제 std::cin 으로 읽으면 oss 에 담긴 내용이 들어온다
   std::string line;
   int cnt = 0;
   while (std::getline(std::cin, line)) {
@@ -198,7 +208,6 @@ TEST(LoggerTest, FileAndConsoleLogTest) {
     cnt++;
   }
 
-  // 6) std::cin 버퍼도 복원
   std::cin.rdbuf(old_cin_buf);
 
   const std::string file_path = "file_console_file_log_test.txt";
@@ -206,7 +215,7 @@ TEST(LoggerTest, FileAndConsoleLogTest) {
 
   EXPECT_TRUE(ifs.is_open());
 
-  for (uint i =0;i < logs.size();i++) {
+  for (uint i = 0; i < logs.size(); i++) {
     std::string line;
     if (std::getline(ifs, line)) {
       EXPECT_TRUE(line.find(logs[i]) != std::string::npos);
@@ -218,11 +227,18 @@ TEST(LoggerTest, LogLevelTest) {
   Logger lg;
   lg.setLevel(LogLevel::kInfo);
   lg.clearSink();
-  lg.addSink(std::make_unique<FileSink>("LogLevelTest", 1024*1024));
+  lg.addSink(std::make_unique<FileSink>("LogLevelTest", 1024 * 1024));
 
-  for (int i = 0 ; i < 200;i++) {
-    lg.info("info LogLevelTest" + std::to_string(i));
+  for (int i = 0; i < 200; i++) {
+    lg.debug("info LogLevelTest" + std::to_string(i));
   }
 
   sleep(3);
+
+  const std::string file_path = "LogLevelTest.txt";
+  std::ifstream ifs(file_path);
+  EXPECT_TRUE(ifs.is_open());
+  std::string line;
+  std::getline(ifs, line);
+  EXPECT_TRUE(line.empty());
 }
